60a97f178cb44b148f20dba6d6c160c5ce6dc1a3
Yuchen Liu <lyc990312@hotmail.com>
2024-05-16 15:57:59 +1000

    Add the post.js, which contain the post schema

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-const express = requrie("express")
+const express = require("express")
 const { ApolloServer } = require("apollo-server-express")
 const mongoose = require("mongoose")
+const { gql } = require("apollo-server-express")
+const logger = require("morgan")
+
+require("dotenv").config()
 
 // connect to the mongodb
-const URL = process.env.MONGODB_URL
+const URL = process.env.DATABASE_URL
 mongoose.connect(URL)
 const db = mongoose.connection
 
@@ -12,9 +16,25 @@ db.on('connected', function () {
     console.log(`Connected to ${db.name} at ${db.host}:${db.port}`)
 })
 
+// queries
+const typeDefs = gql`
+    type Query {
+        hello:String
+    }
+`
+// resolvers
+const resolvers = {
+    Query: {
+        hello: () => {
+            return "Hello world"
+        }
+    }
+}
+
 const startServer = async () => {
     // start express
     const app = express()
+    app.use(logger("dev"))
     const port = process.env.PORT || 4000
     // start server
     const apolloServer = new ApolloServer({
@@ -22,7 +42,7 @@ const startServer = async () => {
         resolvers
     })
     await apolloServer.start()
-    apolloServer.applyMiddleware({app: app})
+    apolloServer.applyMiddleware({ app: app })
     app.listen(port, function () {
         console.log(`Express app running on port ${port}`)
     })
