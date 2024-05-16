b8b74b99f5ed03f742b9149e4dd80952e49fefc8
Yuchen Liu <lyc990312@hotmail.com>
2024-05-16 14:53:10 +1000

    Finish the basic server setup

--- server.js
+++ server.js
@@ -1,4 +1,31 @@
 const express = requrie("express")
-const {ApolloServer} = require("apollo-server-express")
+const { ApolloServer } = require("apollo-server-express")
 const mongoose = require("mongoose")
 
+// connect to the mongodb
+const URL = process.env.MONGODB_URL
+mongoose.connect(URL)
+const db = mongoose.connection
+
+// console log when success connected to the mongodb
+db.on('connected', function () {
+    console.log(`Connected to ${db.name} at ${db.host}:${db.port}`)
+})
+
+const startServer = async () => {
+    // start express
+    const app = express()
+    const port = process.env.PORT || 4000
+    // start server
+    const apolloServer = new ApolloServer({
+        typeDefs,
+        resolvers
+    })
+    await apolloServer.start()
+    apolloServer.applyMiddleware({app: app})
+    app.listen(port, function () {
+        console.log(`Express app running on port ${port}`)
+    })
+}
+
+startServer()
